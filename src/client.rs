1703ce50d5ca66d36fe938ea1edf35258f875ab2
GunnarMorrigan <13799935+GunnarMorrigan@users.noreply.github.com>
2023-03-24 10:27:25 +0100

    Added blocking calls for client
    These calls should technically not block unless there are too many inflight messages

--- src/client.rs
+++ src/client.rs
@@ -19,6 +19,8 @@ pub struct MqttClient {
     to_network_s: Sender<Packet>,
 }
 
+/// Async functions to perform MQTT operations
+#[cfg(any(feature = "tokio", feature = "smol"))]
 impl MqttClient {
     pub fn new(available_packet_ids: Receiver<u16>, to_network_s: Sender<Packet>) -> Self {
         Self { available_packet_ids, to_network_s }
@@ -115,6 +117,104 @@ impl MqttClient {
     pub async fn disconnect_with_properties(&self, reason_code: DisconnectReasonCode, properties: DisconnectProperties) -> Result<(), ClientError> {
         let disconnect = Disconnect { reason_code, properties };
         self.to_network_s.send(Packet::Disconnect(disconnect)).await.map_err(|_| ClientError::NoNetwork)?;
+        Ok(())
+    }
+}
+
+/// Sync functions to perform MQTT operations
+#[cfg(feature = "sync")]
+impl MqttClient{
+    pub fn subscribe_blocking<A: Into<Subscription>>(&self, into_subscribtions: A) -> Result<(), ClientError> {
+        let pkid = self.available_packet_ids.recv_blocking().map_err(|_| ClientError::NoNetwork)?;
+        let subscription: Subscription = into_subscribtions.into();
+        let sub = Subscribe::new(pkid, subscription.0);
+        self.to_network_s.send_blocking(Packet::Subscribe(sub)).map_err(|_| ClientError::NoNetwork)?;
+        Ok(())
+    }
+
+    pub fn subscribe_with_properties_blocking<S: Into<Subscription>>(&self, properties: SubscribeProperties, into_sub: S) -> Result<(), ClientError> {
+        let pkid = self.available_packet_ids.recv_blocking().map_err(|_| ClientError::NoNetwork)?;
+        let sub = Subscribe {
+            packet_identifier: pkid,
+            properties,
+            topics: into_sub.into().0,
+        };
+        self.to_network_s.send_blocking(Packet::Subscribe(sub)).map_err(|_| ClientError::NoNetwork)?;
+        Ok(())
+    }
+
+    pub fn publish_blocking<P: Into<Bytes>>(&self, qos: QoS, retain: bool, topic: String, payload: P) -> Result<(), ClientError> {
+        let pkid = match qos {
+            QoS::AtMostOnce => None,
+            _ => Some(self.available_packet_ids.recv_blocking().map_err(|_| ClientError::NoNetwork)?),
+        };
+        info!("Published message with ID: {:?}", pkid);
+        let publish = Publish {
+            dup: false,
+            qos,
+            retain,
+            topic,
+            packet_identifier: pkid,
+            publish_properties: PublishProperties::default(),
+            payload: payload.into(),
+        };
+        self.to_network_s.send_blocking(Packet::Publish(publish)).map_err(|_| ClientError::NoNetwork)?;
+        info!("Published message into handler_packet_sender: len {}", self.to_network_s.len());
+        Ok(())
+    }
+
+    pub fn publish_with_properties_blocking<P: Into<Bytes>>(&self, qos: QoS, retain: bool, topic: String, payload: P, properties: PublishProperties) -> Result<(), ClientError> {
+        let pkid = match qos {
+            QoS::AtMostOnce => None,
+            _ => Some(self.available_packet_ids.recv_blocking().map_err(|_| ClientError::NoNetwork)?),
+        };
+        let publish = Publish {
+            dup: false,
+            qos,
+            retain,
+            topic,
+            packet_identifier: pkid,
+            publish_properties: properties,
+            payload: payload.into(),
+        };
+        self.to_network_s.send_blocking(Packet::Publish(publish)).map_err(|_| ClientError::NoNetwork)?;
+        Ok(())
+    }
+
+    pub fn unsubscribe_blocking<T: Into<UnsubscribeTopics>>(&self, into_topics: T) -> Result<(), ClientError> {
+        let pkid = self.available_packet_ids.recv_blocking().map_err(|_| ClientError::NoNetwork)?;
+        let unsub = Unsubscribe {
+            packet_identifier: pkid,
+            properties: UnsubscribeProperties::default(),
+            topics: into_topics.into().0,
+        };
+        self.to_network_s.send_blocking(Packet::Unsubscribe(unsub)).map_err(|_| ClientError::NoNetwork)?;
+        Ok(())
+    }
+
+    pub fn unsubscribe_with_properties_blocking<T: Into<UnsubscribeTopics>>(&self, into_topics: T, properties: UnsubscribeProperties) -> Result<(), ClientError> {
+        let pkid = self.available_packet_ids.recv_blocking().map_err(|_| ClientError::NoNetwork)?;
+        let unsub = Unsubscribe {
+            packet_identifier: pkid,
+            properties,
+            topics: into_topics.into().0,
+        };
+        self.to_network_s.send_blocking(Packet::Unsubscribe(unsub)).map_err(|_| ClientError::NoNetwork)?;
+        Ok(())
+    }
+
+    pub fn disconnect_blocking(&self) -> Result<(), ClientError> {
+        let disconnect = Disconnect {
+            reason_code: DisconnectReasonCode::NormalDisconnection,
+            properties: DisconnectProperties::default(),
+        };
+        self.to_network_s.send_blocking(Packet::Disconnect(disconnect)).map_err(|_| ClientError::NoNetwork)?;
+        Ok(())
+    }
+
+    pub fn disconnect_with_properties_blocking(&self, reason_code: DisconnectReasonCode, properties: DisconnectProperties) -> Result<(), ClientError> {
+        let disconnect = Disconnect { reason_code, properties };
+        self.to_network_s.send_blocking(Packet::Disconnect(disconnect)).map_err(|_| ClientError::NoNetwork)?;
         Ok(())
     }
 }
