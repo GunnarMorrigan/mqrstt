b615567ae904c5104cd4fa7eab1f6711bdd34261
GunnarMorrigan <13799935+GunnarMorrigan@users.noreply.github.com>
2024-11-26 23:46:29 +0100

    Fix cargo lint in fuzzing

--- fuzz/fuzz_targets/fuzz_target_1.rs
+++ fuzz/fuzz_targets/fuzz_target_1.rs
@@ -4,11 +4,9 @@ use libfuzzer_sys::fuzz_target;
 
 #[tokio::main(flavor = "current_thread")]
 async fn test(mut data: &[u8]) {
-    mqrstt::packets::Packet::async_read(&mut data).await;
+    let _ = mqrstt::packets::Packet::async_read(&mut data).await;
 }
 
 fuzz_target!(|data: &[u8]| {
-    // let mut packet = bytes::BytesMut::from(data);
-    // mqrstt::packets::Packet::read_from_buffer(&mut packet);
     test(data);
 });
