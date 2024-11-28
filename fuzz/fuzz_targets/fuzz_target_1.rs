634e05b81f87fa767f7d0c3090bec2e3c0cdb4c1
GunnarMorrigan <13799935+GunnarMorrigan@users.noreply.github.com>
2024-11-28 11:25:19 +0100

    Disable fuzzing crate, gives errors in windows

--- fuzz/fuzz_targets/fuzz_target_1.rs
+++ fuzz/fuzz_targets/fuzz_target_1.rs
@@ -1,7 +1,9 @@
 #![no_main]
 
+#[cfg(target_os = "linux")]
 use libfuzzer_sys::fuzz_target;
 
+#[cfg(target_os = "linux")]
 #[tokio::main(flavor = "current_thread")]
 async fn test(mut data: &[u8]) {
     let _ = mqrstt::packets::Packet::async_read(&mut data).await;
